fccd3497a703d43e6754ce4f09829ad8f198d229
mutuzogabin <shemamutuzogabin250@gmail.com>
2025-09-27 09:36:47 +0200

    Add files via upload

---
+++ Individual assignment 22 september 2025/calculator/src/com/TestRegistration/TestRegistration.java
@@ -0,0 +1,14 @@
+package com.TestRegistration;
+
+import com.Registration.RegistrationProject;
+
+public class TestRegistration {
+
+	public static void main(String[] args) {
+		
+		RegistrationProject arr = new RegistrationProject();
+		// TODO Auto-generated method stub
+
+	}
+
+}
