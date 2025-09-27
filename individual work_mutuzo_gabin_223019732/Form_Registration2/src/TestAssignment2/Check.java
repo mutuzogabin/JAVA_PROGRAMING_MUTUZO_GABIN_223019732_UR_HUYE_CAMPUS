3529d2b73c303de57ea03f7b2062957a7bc4d636
mutuzogabin <shemamutuzogabin250@gmail.com>
2025-09-27 08:07:02 +0200

    Add files via upload

---
+++ individual work_mutuzo_gabin_223019732/Form_Registration2/src/TestAssignment2/Check.java
@@ -0,0 +1,13 @@
+package TestAssignment2;
+
+import form.individual.Assignment2;
+
+public class Check {
+
+	public static void main(String[] args) {
+		Assignment2 justine = new Assignment2();
+		
+
+	}
+
+}
